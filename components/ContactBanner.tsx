3a3701b482ae470106f1acdb3447515fa66520dd
jambumoses <jambcareer@gmail.com>
2025-05-08 13:05:00 +0300

    contact heading

--- components/ContactBanner.tsx
+++ components/ContactBanner.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function ContactBanner() {
   return (
-    <div className="h-[80vh]">
+    <div className="h-[80vh] flex flex-col justify-center items-center">
         {/* banner */}
+        <h1 className='capitalize text-[50px] font-bold'>contact us today</h1>
       </div>
   )
 }
